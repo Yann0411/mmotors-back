fd499c6540e69a7c43a9dcbac6e5fd668ff6095a
Yann Christophe <ychristophe@outlook.fr>
2026-05-17 02:49:05 +0200

    ajout de l'url front de netlify dans securityConfig

--- src/main/java/com/mmotors/mmotors/SecurityConfig.java
+++ src/main/java/com/mmotors/mmotors/SecurityConfig.java
@@ -51,7 +51,7 @@ public class SecurityConfig {
         System.out.println("=>=>=>=>=>=>=>=>=>JE SUIS DANS LE SECURITY_CONFIG<=<=<=<=<=<=<=<=<=<=<=<=<=<=<=");
         System.out.println("=>=>=>=>=>=>=>=>=>=> CORS_CONFIGURATION_SOURCE<=<=<=<=<=<=<=<=<=<=<=<=<=<=<=<=");
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://127.0.0.1:5500", "http://localhost:5500"));
+        config.setAllowedOrigins(List.of("http://127.0.0.1:5500", "http://localhost:5500", "https://charming-tarsier-05f0f3.netlify.app"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
         config.setAllowedHeaders(List.of("*"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
