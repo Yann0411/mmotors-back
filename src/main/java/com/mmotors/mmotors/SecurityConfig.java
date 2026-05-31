a2ce4e236fbfc5d0751a9989af328407182c24da
Yann Christophe <ychristophe@outlook.fr>
2026-05-31 22:59:43 +0200

    ajout de PATCH dans securitéConfig

--- src/main/java/com/mmotors/mmotors/SecurityConfig.java
+++ src/main/java/com/mmotors/mmotors/SecurityConfig.java
@@ -52,7 +52,7 @@ public class SecurityConfig {
         System.out.println("=>=>=>=>=>=>=>=>=>=> CORS_CONFIGURATION_SOURCE<=<=<=<=<=<=<=<=<=<=<=<=<=<=<=<=");
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOrigins(List.of("http://127.0.0.1:5500", "http://localhost:5500", "https://charming-tarsier-05f0f3.netlify.app"));
-        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE" , "PATCH"));
         config.setAllowedHeaders(List.of("*"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", config);
